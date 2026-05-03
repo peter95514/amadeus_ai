135c4628ff1bf38d51c50b8b30bc78ab9da2e5f7
peter95514 <peter95514@gmail.com>
2026-05-03 09:15:31 +0800

    for some ignore file

--- app/build/CMakeFiles/Amadeus.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Amadeus.
-# This may be replaced when dependencies are built.
--- app/build/CMakeFiles/Amadeus.dir/src/main.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/Amadeus.dir/src/main.cpp.o: \
- /home/peter/amadeus_ai/app/src/main.cpp /usr/include/stdc-predef.h
